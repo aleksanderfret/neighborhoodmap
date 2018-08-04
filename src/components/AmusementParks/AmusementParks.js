d82b07036c1a50d2f71ff41f2a8d356386eb6f65
Aleksander Fret <alemik@op.pl>
2018-08-04 12:07:35 +0200

    change google APIi map link

--- src/components/AmusementParks/AmusementParks.js
+++ src/components/AmusementParks/AmusementParks.js
@@ -69,7 +69,7 @@ class AmusementParks extends Component {
             aria-label="map with amusement pars locations"
           >
             <ParkMap
-              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3&libraries=geometry,drawing,places&key=AIzaSyDRYJI7iuE8nySIexxrjMfquYL-pPyLHW8"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDRYJI7iuE8nySIexxrjMfquYL-pPyLHW8"
               loadingElement={<div className={'loadingElement'} />}
               containerElement={<div className={'containerElement'} />}
               mapElement={<div className={'mapElement'} />}
